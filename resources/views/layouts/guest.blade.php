e8ccf9bf3c4deaada4e7302a9e7a128771f933c3
tombomeke-ehb <tom.dekoning@student.ehb.be>
2026-01-02 06:06:21 +0100

    Refactor guest layout and remove custom font
    Removed Google Fonts Montserrat and related inline styles from the guest layout. Updated the structure and classes for authentication pages, replacing the application logo with a FontAwesome icon and 'Portfolio' text. Also removed the modal.js script from the includes.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,12 +10,6 @@
         <!-- FontAwesome voor iconen -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-        <!-- EIGEN FONTS (voorbeeld: Google Fonts) -->
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet">
-        <style>
-            body, html { font-family: 'Montserrat', Arial, sans-serif !important; }
-        </style>
-
         <!-- EIGEN CSS -->
         <link rel="stylesheet" href="{{ asset('css/base.css') }}">
         <link rel="stylesheet" href="{{ asset('css/components.css') }}">
@@ -26,22 +20,22 @@
 
         <!-- Custom JS -->
         <script src="{{ asset('js/language.js') }}" defer></script>
-        <script src="{{ asset('js/modal.js') }}" defer></script>
         <script src="{{ asset('js/script.js') }}" defer></script>
 
         <script>
             {!! 'window.portfolioTranslations = ' . json_encode(config('translations')) . ';' !!}
         </script>
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <body>
+        <div class="auth-layout">
+            <div class="auth-container">
+                <div class="auth-header">
+                    <div class="auth-logo">
+                        <a href="/">
+                            <i class="fas fa-code"></i> Portfolio
+                        </a>
+                    </div>
+                </div>
                 {{ $slot }}
             </div>
         </div>
